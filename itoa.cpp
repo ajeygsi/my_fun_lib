140402cbc95ee0a6ef7654c622d4ffa11943a975
ajey <ajey@chronus.com>
2011-08-02 19:20:50 +0530

    Added the function itoa.

--- itoa.cpp
+++ itoa.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
+#include<cstdio>
 using namespace std;
 
 string itoa(int n)
@@ -20,6 +21,21 @@ string itoa(int n)
       n = n/10;
       str.push_back('0' + digit);
     }
+  
+  reverse(str.begin(),str.end());
+  return str;
+}
+
+main()
+{
+  int n;
+  printf("Enter a number : ");
+  scanf("%d",&n);
+  printf("Number as a string is : ");
+  cout<<itoa(n)<<endl;
+}
+
+  
   
   
   
